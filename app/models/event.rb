5ac3c95642bd484e88cdf80eb17ea66dfbb44847
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-12-25 19:33:43 +0300

    class Event modified. Views added. Simple search form added

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,41 @@ class Event < ApplicationRecord
 
   has_many :comments, as: :commentable
   has_many :commentators, through: :comments, source: :user
+
+
+    def self.search(search)
+
+
+      @search_city = search[:search_city]
+      @search_start_beg = search[:search_start_beg].to_date
+      @search_start_end = search[:search_start_end].to_date
+      @search_subject = search[:search_subject]
+      #p start_time_search_result.compact
+
+      #p city_search_result.compact
+      #p subject_search_result.compact
+
+      p start_time_search_result.compact & subject_search_result.compact & city_search_result.compact
+    end
+
+    def self.city_search_result
+      self.all.map do |object|
+              object.id if object.city == @search_city
+      end
+    end
+
+    def self.subject_search_result
+      self.all.map do |object|
+        object.id if (object.title).include? @search_subject
+      end
+    end
+
+    def self.start_time_search_result
+      self.all.map do |object|
+        object.id if (object.date_from).between?(@search_start_beg, @search_start_end)
+      end
+    end
 end
+
+
+ #find_by(city: search[:search_city]) if search[:search_city]
--- test.rb
+++ test.rb
@@ -1,1 +1,11 @@
-puts ap User.all.sort
+
+#puts ap Event.first(2)
+data = {search_city: "New Dot", search_subject: "wh", search_start_beg: "25 Dec 2018", search_start_end: "26 Dec 2018"}#, :search_start, :search_end,:search_subject)
+
+#puts data[:search_city]
+
+
+ Event.search(data)
+
+
+#puts ap Event.first.title.include? data[:search_subject]
